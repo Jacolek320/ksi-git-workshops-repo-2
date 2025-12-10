b742a3bf4cdf8efe8f3e8c08699dfc24ae20be17
Łukasz Drukała <masflam@masflam.com>
2025-12-10 11:18:53 +0100

    Squashed Extract function
    Return vector
    
    Include headers
    
    Use std::to_string

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
+#include <string>
 
-void fizz_buzz(int n) {
+std::vector<std::string> fizz_buzz(int n) {
+  std::vector<std::string> output;
+
   for (int i = 1; i <= n; ++i) {
     if (i % 3 == 0 && i % 5 == 0) {
-      std::cout << "FizzBuzz" << std::endl;
+      output.push_back("FizzBuzz");
     } else if (i % 3 == 0) {
-      std::cout << "Fizz" << std::endl;
+      output.push_back("Fizz");
     } else if (i % 5 == 0) {
-      std::cout << "Buzz" << std::endl;
+      output.push_back("Buzz");
     } else {
-      std::cout << i << std::endl;
+      output.push_back(std::to_string(i));
     }
   }
+
+  return output;
 }
 
 int main() {
